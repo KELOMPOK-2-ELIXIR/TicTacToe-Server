64bd368a626eb16c80676b89b1a6b4bf0ea0c681
Cokro Siamdani <cokrosiamdani@gmail.com>
2020-05-14 13:06:11 +0700

    user

---
+++ migrations/20200514054540-create-user.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      room: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      row: {
+        type: Sequelize.INTEGER
+      },
+      column: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+
+  const Model = sequelize.Sequelize.Model;
+
+  class User extends Model{}
+  
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: "Username is required"
+        },
+        notEmpty: {
+          args: true,
+          msg: "Username is required"
+        }
+      }
+    },
+    room: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: "Room is required"
+        },
+        notEmpty: {
+          args: true,
+          msg: "Room is required"
+        }
+      }
+    },
+    row: {
+      type: DataTypes.INTEGER
+    },
+    column: {
+      type: DataTypes.INTEGER
+    }
+  },
+  {
+    sequelize,
+    modelName: "User"
+  });
+  
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
